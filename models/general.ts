6345f0cd2b0b493bee06886304f267d410259ab4
Tannaz <tia.shr97@gmail.com>
2022-12-20 11:56:16 +0330

    adding dummy data

---
+++ models/general.ts
@@ -0,0 +1,13 @@
+
+export type gender = "female" | "male" | "both";
+
+export interface IWatch {
+  id: string;
+  title: string;
+  description?: string;
+  isFeatured: boolean;
+  price: number;
+  gender: gender;
+  productCode: string;
+  image: string;
+}
